4b6874bf78fb1d7ce6239903320028ad2e13727b
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-01-07 22:02:55 +0800

    medical staff

---
+++ app/Console/Commands/SyncTask.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Threshold;
+use App\User;
+use Illuminate\Console\Command;
+
+class SyncTask extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'syncTask';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'sync all the offline data to server';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $test ="test";
+        $terminal = new Threshold();
+        $terminal->causer_id = '42a5dc8a-deeb-4569-ac0e-fc8b45db0782';
+        $terminal->data = User::find('42a5dc8a-deeb-4569-ac0e-fc8b45db0782');
+        $terminal->action = "test only";
+        $terminal->save();
+    }
+}
